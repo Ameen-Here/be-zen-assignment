df4a1f25ab6a3f0acbb8f2f65b054037638a3c48
Ameen-Here <johnharry125@yahoo.com>
2022-09-12 16:29:29 +0530

    Added basic css and variables

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import Header from "./components/header/Header";
 
 function App() {
-  return <div className="App">app</div>;
+  return (
+    <div className="container">
+      <Header />
+    </div>
+  );
 }
 
 export default App;
