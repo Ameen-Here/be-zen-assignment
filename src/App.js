616fdae9483835c9ff1540fb9af0f104592ff0a3
Ameen-Here <johnharry125@yahoo.com>
2022-09-12 21:14:32 +0530

    Added overlay add recipe components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import "./App.css";
 import Header from "./components/header/Header";
+import AddRecipeOverlay from "./components/Overlay/AddRecipeOverlay";
 import RecipeViewer from "./components/recipeViewer/RecipeViewer";
 import ResultsView from "./components/resultsView/ResultsView";
 
 function App() {
   return (
-    <div className="container">
-      <Header />
-      <ResultsView />
-      <RecipeViewer />
-    </div>
+    <>
+      <div className="container">
+        <Header />
+        <ResultsView />
+        <RecipeViewer />
+      </div>
+      <AddRecipeOverlay />
+    </>
   );
 }
 
