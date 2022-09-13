564121a71f787563bfaccf4e01e1299c9d87e56a
Ameen-Here <johnharry125@yahoo.com>
2022-09-13 21:00:27 +0530

    Connected to mongo server through mongoose

--- server/server.js
+++ server/server.js
@@ -2,6 +2,20 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 
+const mongoose = require("mongoose");
+
+// Connecting Mongoose to mongo
+
+mongoose
+  .connect("mongodb://localhost:27017/recipeez", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connection established with mongoose"))
+  .catch((err) => console.log(`Error in connecting with mongoose ${err}`));
+
+mongoose.connection.on("error", (err) => logError(err));
+
 const port = process.env.PORT || 3001;
 
 app.use(express.json());
